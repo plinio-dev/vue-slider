e8ddae0f4019ce834d40796f50b364e82fdb8cae
Tommaso P <tommaso.plinio@gmail.com>
2021-03-25 17:25:02 +0100

    prev & next

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-var app = new Vue ({
-  el: '#root',
+var app = new Vue (
+  {
+    el: "#app",
     data: {
-      images: [
-        "https://i.pinimg.com/originals/8c/52/81/8c52811a28dc44c8f7950502131162a5.jpg",
-        "https://www.turismo.it/typo3temp/pics/c96fc8b49a.jpg",
-        "https://molisecoast.com/wp-content/uploads/2017/06/trabucchi-termoli-41-1024x685.jpg"
-      ],
-      imageIndex : 0
+        images: [
+          "https://i.pinimg.com/originals/8c/52/81/8c52811a28dc44c8f7950502131162a5.jpg",
+          "https://www.turismo.it/typo3temp/pics/c96fc8b49a.jpg",
+          "https://molisecoast.com/wp-content/uploads/2017/06/trabucchi-termoli-41- 1024x685.jpg"
+        ],
+        imageIndex : 0
     },
+    methods : {
+      nextImage: function() {
+        this.imageIndex++;
 
+        if (this.imageIndex == this.images.lenght) {
+          this.imageIndex = 0;
+        }
+      },
+      prevImage : function() {
+        this.imageIndex--;
 
-
-
+        if (this.imageIndex < 0) {
+          this.imageIndex = this.images.lenght - 1;
+        }
+      }
+    }
   }
 );

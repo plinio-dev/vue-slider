1d10ceb7a8ea76c749a28a9742c3184ef0cb88b6
Tommaso P <tommaso.plinio@gmail.com>
2021-03-25 17:00:55 +0100

    immagini inserite nel carousel

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var app = new Vue ({
+  el: '#root',
+    data: {
+      images: [
+        "https://i.pinimg.com/originals/8c/52/81/8c52811a28dc44c8f7950502131162a5.jpg",
+        "https://www.turismo.it/typo3temp/pics/c96fc8b49a.jpg",
+        "https://molisecoast.com/wp-content/uploads/2017/06/trabucchi-termoli-41-1024x685.jpg"
+      ],
+      imageIndex : 0
+    },
+
+
+
+
+  }
+);
